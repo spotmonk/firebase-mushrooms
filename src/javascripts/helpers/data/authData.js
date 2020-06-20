a4d65eb85f904f531e846c2c3ad6c5442e239a6f
Geno McNew <genemcnew3@gmail.com>
2020-06-20 11:58:41 -0500

    google auth works q

---
+++ src/javascripts/helpers/data/authData.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const authDiv = $('#auth');
+const forestDiv = $('#forest');
+const logoutButton = $('#navbar-logout-button');
+
+const checkLogInStatus = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      authDiv.addClass('hide');
+      forestDiv.removeClass('hide');
+      logoutButton.removeClass('hide');
+    } else {
+      authDiv.removeClass('hide');
+      forestDiv.addClass('hide');
+      logoutButton.addClass('hide');
+    }
+  });
+};
+
+export default { checkLogInStatus };
