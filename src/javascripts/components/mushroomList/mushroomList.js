e02604ab1de98f7a421cb0181d9d318be8ce876a
Geno McNew <genemcnew3@gmail.com>
2020-06-22 18:50:40 -0500

    created promise for mushrooms

--- src/javascripts/components/mushroomList/mushroomList.js
+++ src/javascripts/components/mushroomList/mushroomList.js
@@ -2,7 +2,7 @@ import mushroomData from '../../helpers/data/mushroomData';
 
 const buildForest = () => {
   mushroomData.getMushrooms()
-    .then((response) => console.warn('get Mushrooms Worked', response.data))
+    .then((mushrooms) => console.warn('get Mushrooms Worked', mushrooms))
     .catch((err) => console.error('get mushrooms broke :(', err));
 };
 
--- src/javascripts/helpers/data/mushroomData.js
+++ src/javascripts/helpers/data/mushroomData.js
@@ -3,6 +3,18 @@ import apiKeys from '../apiKeys.json';
 
 const baseURL = apiKeys.firebaseConfig.databaseURL;
 
-const getMushrooms = () => axios.get(`${baseURL}/mushrooms.json`);
+const getMushrooms = () => new Promise((resolve, reject) => {
+  axios.get(`${baseURL}/mushrooms.json`)
+    .then((response) => {
+      const mushroomObjects = response.data;
+      const mushrooms = [];
+      Object.keys(mushroomObjects).forEach((mushroomId) => {
+        mushroomObjects[mushroomId].id = mushroomId;
+        mushrooms.push(mushroomObjects[mushroomId]);
+      });
+      resolve(mushrooms);
+    })
+    .catch((err) => reject(err));
+});
 
 export default { getMushrooms };
